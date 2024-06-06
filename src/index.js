7d08c731406f27bb4f5eed2ca1237ba2dafe5c42
MaheshwariButle07 <maheshwaributle07@gmail.com>
2024-06-06 13:32:50 +0530

    updated

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDom from "react-dom/client"
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+
+root.render(
+    <>
+    <h1>This is my First React Code</h1>
+    </>
+)
